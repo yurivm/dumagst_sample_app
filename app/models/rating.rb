6cb3c3686dfe5009785125637059a1d631da93a3
yurivm <yuri.veremeyenko@gmail.com>
2014-11-09 22:17:33 +0100

    add the users#show view with rated movies

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,6 @@
 class Rating < ActiveRecord::Base
   belongs_to :user
   belongs_to :movie
+
+  scope :by_rating_desc, -> { order(rating: :desc)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :ratings
   has_many :rated_movies, through: :ratings, source: :movie
+
 end
